9c3566fd3262bcef3f3ba830dac9d0986e51b2df
jiradamugonshi <majdisaghrouni2005@gmail.com>
2024-10-22 21:53:51 -0400

    Update gh_repo_issue.sh

--- Scripts/gh_repo_issue.sh
+++ Scripts/gh_repo_issue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Create an array
-dir_array_mirantis=() #("fuel-plugin-celebrer" "pmlc_cinder" "pmlc_keystone" "pmlc_neutron" "pmlc_nova" "profiles" "puppet-deployapp" "puppet-foreman_proxy"  "puppet-haproxy" "puppet-manifests" "puppet-module-keepalived"  "puppet-sudo" "puppetlabs-cinder" "puppetlabs-glance" "puppetlabs-mysql" "puppetlabs-nova" "puppetlabs-swift" "roles" "solar" "solar-resources")
-dir_array_wikimedia=() #("analytics-vagrant-kraken" "operations-puppet")
+dir_array_mirantis=("fuel-plugin-celebrer" "pmlc_cinder" "pmlc_keystone" "pmlc_neutron" "pmlc_nova" "profiles" "puppet-deployapp" "puppet-foreman_proxy"  "puppet-haproxy" "puppet-manifests" "puppet-module-keepalived"  "puppet-sudo" "puppetlabs-cinder" "puppetlabs-glance" "puppetlabs-mysql" "puppetlabs-nova" "puppetlabs-swift" "roles" "solar" "solar-resources")
+dir_array_wikimedia=("analytics-vagrant-kraken" "operations-puppet")
 dir_array_mozilla=("cheesecake")
 
 # File to store the output
