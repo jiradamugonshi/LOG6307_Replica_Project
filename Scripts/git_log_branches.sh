153e4de9e25ac6d883613dd8690fe00f81015bd1
jiradamugonshi <majdisaghrouni2005@gmail.com>
2024-10-22 21:48:51 -0400

    Update git_log_branches.sh

--- Scripts/git_log_branches.sh
+++ Scripts/git_log_branches.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create an array
-#dir_array=("fuel-plugin-celebrer/" "fuel-plugin-external-ceph/" "pmlc_cinder/" "pmlc_keystone/" "pmlc_neutron/" "pmlc_nova/" "profiles/" "puppet-deployapp/" "puppet-foreman_proxy/" "puppet-haproxy/" "puppet-kafka/" "puppetlabs-apt/" "puppetlabs-cinder/" "puppetlabs-glance/" "puppetlabs-keystone/" "puppetlabs-mysql/" "puppetlabs-nova/" "puppetlabs-openstack/" "puppetlabs-rsync/" "puppetlabs-swift/" "puppet-manifests/" "puppet-mmm/" "puppet-module-keepalived/" "puppet-savanna/" "puppet-ssh/" "puppet-sudo/" "roles/" "solar/" "solar-resources/")
-#dir_array=("analytics-vagrant-kraken/" "mediawiki-vagrant/" "operations-puppet/" "operations-puppet-cdh4/" "operations-puppet-kafka/" "operations-puppet-mariadb/" "operations-puppet-wikimetrics/")
-#dir_array=("cheesecake/" "oldpto/" "opsec-puppet" "socorro-infra/")
-dir_array=("devstack-vagrant/" "puppet-barbican/" "puppet-ceph/" "puppet-cinder/" "puppet-cloudkitty/" "puppet-designate/" "puppet-glance/" "puppet-gnocchi/" "puppet-heat/" "puppet-ironic/" "puppet-magnum/" "puppet-manila/" "puppet-mistral/" "puppet-octavia/" "puppet-openstack-cookiecutter/" "puppet-openstack-integration/" "puppet-oslo/" "puppet-placement/" "puppet-swift/" "puppet-vitrage/" "puppet-watcher/" "puppet-zaqar/")
+#dir_array_mirantis=("fuel-plugin-celebrer/" "fuel-plugin-external-ceph/" "pmlc_cinder/" "pmlc_keystone/" "pmlc_neutron/" "pmlc_nova/" "profiles/" "puppet-deployapp/" "puppet-foreman_proxy/" "puppet-haproxy/" "puppet-kafka/" "puppetlabs-apt/" "puppetlabs-cinder/" "puppetlabs-glance/" "puppetlabs-keystone/" "puppetlabs-mysql/" "puppetlabs-nova/" "puppetlabs-openstack/" "puppetlabs-rsync/" "puppetlabs-swift/" "puppet-manifests/" "puppet-mmm/" "puppet-module-keepalived/" "puppet-savanna/" "puppet-ssh/" "puppet-sudo/" "roles/" "solar/" "solar-resources/")
+#dir_array_wikimedia=("analytics-vagrant-kraken/" "mediawiki-vagrant/" "operations-puppet/" "operations-puppet-cdh4/" "operations-puppet-kafka/" "operations-puppet-mariadb/" "operations-puppet-wikimetrics/")
+#dir_array_mozilla=("cheesecake/" "oldpto/" "opsec-puppet" "socorro-infra/")
+dir_array_openstack=("devstack-vagrant/" "puppet-barbican/" "puppet-ceph/" "puppet-cinder/" "puppet-cloudkitty/" "puppet-designate/" "puppet-glance/" "puppet-gnocchi/" "puppet-heat/" "puppet-ironic/" "puppet-magnum/" "puppet-manila/" "puppet-mistral/" "puppet-octavia/" "puppet-openstack-cookiecutter/" "puppet-openstack-integration/" "puppet-oslo/" "puppet-placement/" "puppet-swift/" "puppet-vitrage/" "puppet-watcher/" "puppet-zaqar/")
 
 
 # File to store the output
@@ -15,7 +15,7 @@ output_file="output.csv"
 
 cd "../Replica/openstack"
 # Loop through the array
-for path in "${dir_array[@]}"; do
+for path in "${dir_array_openstack[@]}"; do
 	cd $path
 	git log --pretty=format:"${PWD##*/};%h;%an;%cs%n" >> "../../../Replica_Project/$output_file" 2>&1
 	cd ../
